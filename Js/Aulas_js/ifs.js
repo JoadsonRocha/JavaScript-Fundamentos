626fe6996af9fe9704e9099ad040490f80f79252
JoadsonRocha <joadsonrocharr@gmail.com>
2020-10-06 09:18:29 -0400

    comandos básicos

--- Js/Aulas_js/ifs.js
+++ Js/Aulas_js/ifs.js
@@ -1,10 +1,10 @@
-let n1 = Number(prompt('Digite um numero: '))
-let n2 = Number(prompt('Digite outro numero: '))
-let n3 = Number(prompt('Digite outro numero: '))
+var n1 = 10     
+var n2 = 20
+var n3 = 30
 
-if (n1 > n2 || n1 > n3)  {
+if (n1 > n2 && n1 > n3)  {
     console.log(`Maior: ${n1}`)
-} else if (n2 > n1 || n2 > n3) {
+} else if (n2 > n1 && n2 > n3) {
     console.log(`Maior ${n2}`)
 } else {
     console.log(`Maior: ${n3}`)
