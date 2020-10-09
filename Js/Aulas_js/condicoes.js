154846a1b5c2da9cfdd02e253008ea085c879d83
JoadsonRocha <joadsonrocharr@gmail.com>
2020-10-08 20:02:13 -0400

    cursos e exercicios

---
+++ Js/Aulas_js/condicoes.js
@@ -0,0 +1,7 @@
+var vel = 100
+
+if (vel > 60 && vel < 70){
+    console.log(`Sua velocidade está acima do permitido: ${vel}`)
+} else {
+    console.log('Velocidade Normal')
+}
---
+++ Js/Aulas_js/condiçoes2.js
@@ -0,0 +1,5 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`A Hora atual é ${hora}`)
+
+if (hora )
