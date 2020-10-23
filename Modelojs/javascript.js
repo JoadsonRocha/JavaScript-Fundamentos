3e4922825cad8b1a439143f79b9a6c5be30e872f
JoadsonRocha <joadsonrocharr@gmail.com>
2020-10-23 10:25:26 -0400

    exercicio 14 completo js

--- Modelojs/javascript.js
+++ Modelojs/javascript.js
@@ -1,18 +1,17 @@
 function carregar (){
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
-var data = new Date()
-var hora = data.getDate()
-msg.innerHTML = `agora são ${hora} Horas.`
-
-if (hora >= 0 && hora <= 12)
-    //Bom dia
-    img.src = 'manha.png'
-} else if (hora >= 12 && hora <= 18){
-    // Boa tarde
-    img.src = 'tarde.png'
-} else {
-//bOA NOITE
-    img.src = 'noite.png'
-}
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `agora são ${hora} Horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'manha.png'  //Bom dia
+        document.body.style.background = '#FFF79E'
+    } else if (hora >= 12 && hora <= 18){
+        img.src = 'tarde.png' // Boa tarde
+        document.body.style.background = '#FFB787'
+    } else {
+        img.src = 'noite.png' //boa NOITE
+        document.body.style.background = '#525556'
+    }
 }
