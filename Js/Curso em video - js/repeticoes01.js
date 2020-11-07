e4217152c560a6164819f7577d89516f171d9a34
JoadsonRocha <joadsonrocharr@gmail.com>
2020-11-07 19:57:05 -0400

    condiçoes 1314

---
+++ Js/Curso em video - js/repeticoes01.js
@@ -0,0 +1,17 @@
+//while
+/*var c = 1
+while (c <= 6){
+    console.log(`Quero Pizza: ${c}`)
+    c++
+}*/
+//do While
+/*var c = 1
+do {
+    console.log(`QueroPizza: ${c}`)
+    c++
+} while(c <= 6)*/
+
+//for while
+for (var c = 1; c <= 5; c++){
+    console.log(c)
+}
