5908b80f9ae827b180ec8e535b92f82265a9f252
JoadsonRocha <joadsonrocharr@gmail.com>
2020-11-17 20:53:21 -0400

    atailização de performace

--- exercio016/javascript.js
+++ exercio016/javascript.js
@@ -1,17 +1,31 @@
-function carregar (){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `agora são ${hora} Horas.`
-    if (hora >= 0 && hora < 12){
-        img.src = 'manha.png'  //Bom dia
-        document.body.style.background = '#FFF79E'
-    } else if (hora >= 12 && hora <= 18){
-        img.src = 'tarde.png' // Boa tarde
-        document.body.style.background = '#FFB787'
-    } else {
-        img.src = 'noite.png' //boa NOITE
-        document.body.style.background = '#525556'
-    }
+
+function contar(){
+    var ini = window.document.getElementById('inicio')
+    var fim = window.document.getElementById('fim')
+    var passo = window.document.getElementById('dinter')
+    var res = window.document.getElementById('res')
+   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+       window.alert('Falta dados!')
+   } else {
+       res.innerHTML = 'Contado...<br> '
+       var i = Number(ini.value)
+       var f = Number(fim.value)
+       var p = Number(passo.value)
+       if (p <= 0){
+           alert('Passo invalido considerando 1!')
+           p = 1
+       }
+       if(i < f){ //contagem crescente
+        for(var c = i;c <= f; c += p){
+            res.innerHTML += `${c} \u{1F449} `
+            }
+        } else { 
+           // contagem descrecente
+           for(var c = i; c >= f; c -= p){
+               res.innerHTML += `${c} \u{1f449}`
+           }              
+       }
+       res.innerHTML += `\u{1F3C1}`
+   }
+      
 }
